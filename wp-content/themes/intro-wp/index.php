a549d8b65cfb10a8e0ecfd81c99f8bc796426965
Toon Van den Bos <toon@whitecube.be>
2024-02-16 16:27:40 +0100

    Added frontend assets using WP

--- wp-content/themes/intro-wp/index.php
+++ wp-content/themes/intro-wp/index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="<?= get_template_directory_uri().'/public/theme.css'; ?>">
+    <script src="<?= get_template_directory_uri().'/public/script.js'; ?>"></script>
 </head>
 <body>
     <h1><?= get_bloginfo('name'); ?></h1>
