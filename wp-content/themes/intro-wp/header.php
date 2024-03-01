923b141923a2f01050f61e30d9d4396744154f9d
Toon Van den Bos <toon@whitecube.be>
2024-03-01 15:18:42 +0100

    Added wp_head & wp_footer

--- wp-content/themes/intro-wp/header.php
+++ wp-content/themes/intro-wp/header.php
@@ -3,7 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title('Le site de la HEPL:'); ?></title>
+    
+    <?php wp_head(); ?>
+
     <link rel="stylesheet" href="<?= dw_asset('theme.css'); ?>">
     <script src="<?= dw_asset('script.js') ?>"></script>
 </head>
