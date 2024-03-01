d7f95ad8ce257c77ac551b656e961a6baabdc907
Toon Van den Bos <toon@whitecube.be>
2024-03-01 15:00:32 +0100

    Added page title

--- wp-content/themes/intro-wp/header.php
+++ wp-content/themes/intro-wp/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title><?php wp_title('Le site de la HEPL:'); ?></title>
     <link rel="stylesheet" href="<?= dw_asset('theme.css'); ?>">
     <script src="<?= dw_asset('script.js') ?>"></script>
 </head>
