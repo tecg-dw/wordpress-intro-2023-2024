7472d198f2867f37b58da374937cfb93d57d3862
Toon Van den Bos <toon@whitecube.be>
2024-03-01 16:22:59 +0100

    Added contact page

---
+++ wp-content/themes/intro-wp/template-contact.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Page "Contact" */ ?>
+<?php get_header(); ?>
+    <?php if(have_posts()): while(have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
+        
+        <main class="about">
+            <header class="about__header">
+                <h1 class="about__title"><?= get_the_title(); ?></h1>
+            </header>
+            <section class="about__intro">
+                <h2 class="sro">Introduction</h2>
+                <?= get_the_content(); ?>
+            </section>
+        </main>
+
+    <?php endwhile; endif; // Fermeture de "The Loop" de Wordpress ?>
+<?php get_footer(); ?>
