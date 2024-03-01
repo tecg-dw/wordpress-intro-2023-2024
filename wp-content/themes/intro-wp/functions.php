983c63c9732fd3ac782d554d775888131a12f09d
Toon Van den Bos <toon@whitecube.be>
2024-03-01 14:28:02 +0100

    Added homepage content injection

--- wp-content/themes/intro-wp/functions.php
+++ wp-content/themes/intro-wp/functions.php
@@ -1,5 +1,11 @@
 <?php
 
+// Désactiver l'éditeur de texte Gutenberg de Wordpress :
+add_filter('use_block_editor_for_post', '__return_false');
+
+// Fonctions propres au thème :
+
+// 1. Charger un fichier "public" (asset/image/css/script/...) pour le front-end.
 function dw_asset(string $file): string
 {
     return get_template_directory_uri() . '/public/' . $file;
--- wp-content/themes/intro-wp/index.php
+++ wp-content/themes/intro-wp/index.php
@@ -9,5 +9,13 @@
 </head>
 <body>
     <h1><?= get_bloginfo('name'); ?></h1>
+    <main class="page">
+        <div class="page__content">
+            <?php if(have_posts()): while(have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
+                <h1><?= get_the_title(); ?></h1>
+                <?= get_the_content(); ?>
+            <?php endwhile; endif; // Fermeture de "The Loop" de Wordpress ?>
+        </div>
+    </main>
 </body>
 </html>
