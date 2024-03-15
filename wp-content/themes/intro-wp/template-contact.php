8ce08c920e97e72ec047552a009755dd95293ab5
Toon Van den Bos <toon@whitecube.be>
2024-03-15 08:44:41 +0100

    Added contact page

--- wp-content/themes/intro-wp/template-contact.php
+++ wp-content/themes/intro-wp/template-contact.php
@@ -2,14 +2,22 @@
 <?php get_header(); ?>
     <?php if(have_posts()): while(have_posts()): the_post(); // Ouverture de "The Loop" de Wordpress ?>
         
-        <main class="about">
-            <header class="about__header">
-                <h1 class="about__title"><?= get_the_title(); ?></h1>
+        <main class="contact">
+            <header class="contact__header">
+                <h1 class="contact__title"><?= get_the_title(); ?></h1>
             </header>
-            <section class="about__intro">
-                <h2 class="sro">Introduction</h2>
-                <?= get_the_content(); ?>
+            <section class="contact__intro">
+                <p>Ici il y aura un formulaire de contract</p>
             </section>
+            <aside class="contact__info">
+                <h2>Mes informations</h2>
+                <dl>
+                    <dt>Mon numéro de téléphone:</dt>
+                    <dd><?= get_field('phone') ?></dd>
+                    <dt>Mon adresse mail:</dt>
+                    <dd><?= get_field('email') ?></dd>
+                </dl>
+            </aside>
         </main>
 
     <?php endwhile; endif; // Fermeture de "The Loop" de Wordpress ?>
