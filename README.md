1b9ac8cc17361459f7d6e96575cde722a2820a4f
Toon Van den Bos <toon@whitecube.be>
2024-03-22 17:00:11 +0100

    Added base front-end workflow

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wordpress-intro-2023-2024
+
+## How to launch front-end workflow
+
+1. Install packages using `yarn` or `npm i`
+2. Launch front-end workflow using `yarn watch` (or `yarn production` when compiling for final production website).
---
+++ webpack.mix.js
@@ -0,0 +1,19 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('wp-content/themes/intro-wp/resources/js/site.js', 'wp-content/themes/intro-wp/public/js')
+    .sass('wp-content/themes/intro-wp/resources/sass/site.scss', 'wp-content/themes/intro-wp/public/css')
+    .copy('wp-content/themes/intro-wp/resources/img', 'wp-content/themes/intro-wp/public/img')
+    .options({
+        processCssUrls: false
+    });
